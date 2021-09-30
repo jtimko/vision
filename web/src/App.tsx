b52bd0d29ce97332ecec8098480a776a50237188
jtimko <justin@jtimko.com>
2021-09-29 23:18:17 -0700

    still no luck

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,35 @@
 import './App.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
 import { useState } from 'react';
 // import Users from './components/Users';
 import Notes from './components/Notes';
 import SendMessage from './components/SendMessage';
+import { setContext } from 'apollo-link-context';
 
 export interface UserData {
   id: number
   message: string
 }
 
+const httpLink = new HttpLink({ uri: "http://localhost:4000" })
+const authLink = setContext(async (req, { headers }) => {
+	const token = localStorage.getItem("token")
+
+	return {
+		...headers,
+		headers: {
+			Authorization: token ? `Bearer ${token}` : null
+		}
+	}
+})
+
+const link = authLink.concat(httpLink as any)
+const client = new ApolloClient({
+	link: link as any,
+	cache: new InMemoryCache()
+})
+
 function App() {
-  
-  const client = new ApolloClient( {
-    uri: "http://localhost:4000",
-    cache: new InMemoryCache(),
-    headers: {
-      //Authorization
-  }
-  })
 
   const [data, setData] = useState<UserData[]>([{
     id: 1,
--- web/src/components/SendMessage.tsx
+++ web/src/components/SendMessage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql, useMutation } from '@apollo/client'
+import { gql, useMutation } from '@apollo/client'
 
-const SendMessage = (props: {saveMessage: any}) => {
+const SendMessage = (props: { saveMessage: any }) => {
 
     const MSG_QUERY = gql`
         mutation AddNotesMutation($note: String!, $id: Int!) {
@@ -10,44 +10,34 @@ const SendMessage = (props: {saveMessage: any}) => {
             }
         }
     `
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYzMjk2OTQ3Mn0.BBspkTy2BByi1O3wH7VNtSzo3RnuceGskaE_qhTVCyE"
-    const client = new ApolloClient( {
-        uri: "http://localhost:4000",
-        cache: new InMemoryCache(),
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-      })
-    
+
     const [addNotes] = useMutation(MSG_QUERY);
     const [msg, setMsg] = useState<string>();
 
-    const dbSave = async() => {
+    const dbSave = async () => {
         try {
-            await addNotes( {variables: {note: msg, id: 1} })
+            await addNotes({ variables: { note: msg, id: 1 } })
         }
         catch (error) {
             console.log(error)
         }
     }
-    
+
     return (
-        <ApolloProvider client={client}>
-            <div>
-                <input
-                    type="text"
-                    placeholder="write your note here"
-                    value={msg}
-                    onChange={(e: React.FormEvent<HTMLInputElement>) => setMsg(e.currentTarget.value)}
-                />
-                <input
-                    type="submit"
-                    onClick={(e) => {e.preventDefault(); dbSave(); props.saveMessage(msg); }}
-                    name="submit"
-                    value="submit"
-                />
-            </div>
-        </ApolloProvider>
+        <div>
+            <input
+                type="text"
+                placeholder="write your note here"
+                value={msg}
+                onChange={(e: React.FormEvent<HTMLInputElement>) => setMsg(e.currentTarget.value)}
+            />
+            <input
+                type="submit"
+                onClick={(e) => { e.preventDefault(); dbSave(); props.saveMessage(msg); }}
+                name="submit"
+                value="submit"
+            />
+        </div>
     )
 }
 
